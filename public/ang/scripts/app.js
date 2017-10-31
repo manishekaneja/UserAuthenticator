05a7d0b1f0c0f5fec7fd7a90885424bd0ad0e653
Manish <manishekaneja@gmail.com>
2017-10-31 17:59:57 +0530

    angl_static_flow

---
+++ public/ang/scripts/app.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myApp
+ * @description
+ * # myApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myApp', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'RegisterCtrl',
+        controllerAs: 'register'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl',
+        controllerAs: 'login'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl',
+        controllerAs: 'contact'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ public/ang/scripts/controllers/register.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myApp.controller:RegisterCtrl
+ * @description
+ * # RegisterCtrl
+ * Controller of the myApp
+ */
+angular.module('myApp')
+  .controller('RegisterCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
